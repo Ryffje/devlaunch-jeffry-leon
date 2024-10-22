6fcab2a76ad99941da31e9e4453cceaf1aa165f5
Jeffry Leon <ryffje@gmail.com>
2024-10-22 10:46:46 -0600

    feat(set): add storing and updating user data file

---
+++ data-structures/map/storing-and-updating-user-data.ts
@@ -0,0 +1,23 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+  
+  function manageUsers(users: Map<number, User>): void {
+    const newUser: User = { id: 1, name: "Jeffry Leon", email: "jeffry@example.com" };
+    users.set(newUser.id, newUser);
+  
+    if (users.has(newUser.id)) {
+      const updatedUser = { ...users.get(newUser.id)!, name: "Jeffry Leon" };
+      users.set(newUser.id, newUser);
+
+    }
+
+    const retrievedUser = users.get(newUser.id);
+    console.log(retrievedUser);
+}
+
+const users = new Map<number, User>();
+manageUsers(users)
+     
