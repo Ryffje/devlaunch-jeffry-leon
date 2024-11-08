7dcbe885bfde4199c997f4cc7e4adaf2eb26d5d9
Jeffry Leon <ryffje@gmail.com>
2024-11-07 19:27:15 -0600

    feat(stacks): add reverse a string file

---
+++ data-structures/stacks/valid-expression.ts
@@ -0,0 +1,31 @@
+function isValidExpression(s: string): boolean {
+    const stack: string [] = [];
+
+    const bracketPair: {[key: string]: string} = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+
+        } else if (char === ')' || char === '}' || char === ']'){
+
+            if (stack.length === 0 || stack[stack.length -1] !== bracketPair [char]){
+                return false;
+            }
+
+            stack.pop();
+        }
+
+        
+    }
+
+    return stack.length === 0;
+}
+
+console.log(isValidExpression("({[]})"));
+console.log(isValidExpression("({[])}"));
+
